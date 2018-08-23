04d904f08ae8cf75ee0245e0c52fb9d9869eb7d9
L00149524 <41998910+L00149524@users.noreply.github.com>
2018-08-23 19:15:16 +0100

    uncessary whitespacing remomved
    removed uncessary whitespacing

--- calc.java
+++ calc.java
@@ -6,21 +6,13 @@ public class calc
        public static void main(String[] args)
        {
               int num1, num2;
-
-
-
-
               Scanner input = new Scanner(System.in);
-
-
               System.out.println("Enter Num 1: ");
               num1 = input.nextInt();
               System.out.println("Enter Num 2: ");
               num2 = input.nextInt();
-
               System.out.println("Answer " + (num1+num2));
-       }
-
+        }
 }
 
          
